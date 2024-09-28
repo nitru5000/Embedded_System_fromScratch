1de463126b66ce62cb5ae9c4f9d68119d84ac788
nitru5000 <abood20411@gmail.com>
2024-09-28 16:55:52 +0200

    Ex2

---
+++ c_basics/Ex2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+ 
+ int i ;
+
+ printf("Enter a integer:");
+ scanf("%d",&i);
+ printf("You entered: %d",i);
+
+    return 0;
+}
