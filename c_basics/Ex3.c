20a01d9e7f824ed5ce1e5272fc032a0db86209de
nitru5000 <abood20411@gmail.com>
2024-09-28 17:14:23 +0200

    Ex3

---
+++ c_basics/Ex3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+ 
+ int i ,j;
+
+ printf("Enter two integers:");
+ scanf("%d %d",&i,&j);
+ printf("sum: %d",(i+j));
+
+    return 0;
+}
