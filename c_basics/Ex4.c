92ea98319dacd9d814e8e3d120ab74ac47082455
nitru5000 <abood20411@gmail.com>
2024-09-28 17:22:44 +0200

    Ex4

---
+++ c_basics/Ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+ 
+ float i ,j;
+
+ printf("Enter two integers:");
+ scanf("%f %f",&i,&j);
+ printf("product: %f",(i*j));
+
+    return 0;
+}
