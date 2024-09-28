5e0c50b113ace870493862506112ff12b61d3247
nitru5000 <abood20411@gmail.com>
2024-09-28 18:12:13 +0200

    Home work

---
+++ c_basics/Home_Work/Ex1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {  
+    int a ;  
+
+    printf("Enter an integre you want to check:");  
+
+    scanf("%d", &a); 
+    
+    if (a%2 == 0) printf("%d is even", a);  
+    else printf("%d is odd", a);
+
+    
+
+    
+    
+    return 0;  
+}  
