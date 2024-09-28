f2fbfc3570f80b473ec8da3d5e865619f7363400
nitru5000 <abood20411@gmail.com>
2024-09-28 17:27:58 +0200

    Ex5

---
+++ c_basics/Ex5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+ 
+ char i ;
+
+ printf("Enter a character:");
+ scanf("%c",&i);
+ printf("ASCII Value of %c: %d",i,i);
+
+    return 0;
+}
