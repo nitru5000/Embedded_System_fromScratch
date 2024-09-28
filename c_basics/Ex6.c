527ec43596588132a5ca162a4869682a3f721fb2
nitru5000 <abood20411@gmail.com>
2024-09-28 17:45:15 +0200

    Ex6

---
+++ c_basics/Ex6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+ 
+ int i , j , temp;
+
+ printf("Enter value of a:");
+ scanf("%d",&i);
+ printf("Enter value of b:");
+ scanf("%d",&j);
+ 
+ temp = i;
+ i = j ;
+ j = temp ;
+
+ printf("After swapping, value of a =  %d \n",i);
+ printf("After swapping, value of b =  %d ",j);
+
+    return 0;
+}
