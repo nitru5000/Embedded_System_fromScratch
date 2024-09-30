5c94180948ba222e916cad6a9aa750375cf2423c
nitru5000 <abood20411@gmail.com>
2024-09-30 09:42:04 +0200

    Home_Work

---
+++ c_basics/Home_Work/HW2_Ex5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {  
+     char a ;  
+
+    printf("Enter a character "); 
+    scanf("%c",&a) ;
+
+     (a>= 'a' &&  a<= 'z') || (a>= 'A' &&  a<= 'Z') ?   printf("%c is an alphapet ", a) : printf("%c is not an alphapet ", a);
+   
+     
+    return 0;  
+}   
