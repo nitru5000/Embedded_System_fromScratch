c0d2f0e997e184fa8cdcc624e52c7feb9cbf315e
nitru5000 <abood20411@gmail.com>
2024-09-30 17:22:02 +0200

    Home_Work3

---
+++ Array_&_String/HW3_Array_Ex1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a[2][2], b[2][2], sum = 0;
+
+    printf("Enter The elemnts of 1st matrix\n");
+
+    for (int i = 0; i <= 1; i++)
+    {
+        for (int j = 0; j <= 1; j++)
+        {
+            printf("Enter element a%d%d: ", i + 1, j + 1);
+            scanf("%f", &a[i][j]);
+        }
+    }
+
+    printf("\nEnter The elemnts of 2nd matrix\n");
+
+    for (int i = 0; i <= 1; i++)
+    {
+        for (int j = 0; j <= 1; j++)
+        {
+            printf("Enter element b%d%d: ", i + 1, j + 1);
+            scanf("%f", &b[i][j]);
+        }
+    }
+
+    printf("sum of matrices :\n");
+
+    for (int i = 0; i <= 1; i++)
+    {
+        for (int j = 0; j <= 1; j++)
+        {
+            printf("%.1f  ", (a[i][j] + b[i][j]));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
