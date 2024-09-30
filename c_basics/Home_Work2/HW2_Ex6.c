7e4f633c15b7bc9f56696bbe5267bd8f071561b4
nitru5000 <abood20411@gmail.com>
2024-09-30 09:51:49 +0200

    Home_work

---
+++ c_basics/Home_Work2/HW2_Ex6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {  
+     int a ,sum =0;;  
+
+      printf("Enter a number "); 
+      scanf("%d",&a) ;
+
+      for (int i =0 ; i <= a ; i++)  sum += i;
+    
+      printf("sum %d " , sum);
+     
+    return 0;  
+}   
